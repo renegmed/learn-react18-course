d76e79bcab509fb032900d6448a55b338ae10d34
Rene Medalla <renemed@pop-os.localdomain>
2022-09-03 14:56:15 -0400

    lifting state and custome events

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import {useState} from 'react';
 import Navbar from './Navbar';
 import List from './List'; 
-import Counter from './CounterClass';
+import AddItem from './AddItem';
+//import Counter from './CounterClass';
 // import Counter from './Counter';
 function App() {
-    const guitars = ['Strat', 'Les Paul', 'Explorer'];
+    const [guitars, setGuitars] = useState(['Strat', 'Les Paul', 'Explorer']);
     
     // sames as pass object as props (read only)
     // const stuff = List({title: 'Guitars', items: guitars});
     // then use as {stuff} instead of <List .../>
 
+    function updateGuitars(newGuitar) {
+        // spread out existing guitars and append new guitar
+        setGuitars([...guitars, newGuitar]); 
+    }
+
     return (
         <>
             <Navbar title="Getting Started with State" />
             <div className="container">
+                <AddItem buttonText="Add Guitar" onSubmit={updateGuitars}/>
                 <List 
                     title="Guitars"
-                    items={guitars}
-                    background='primary'
-                />
-                <Counter />
+                    items={guitars}            
+                />                
             </div>
         </>
         
